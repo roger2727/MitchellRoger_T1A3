1548fceeeae0262ff619651921fce9686fb3b81b
Mitchell Roger <roger.mitch27@gmail.com>
2022-09-14 20:56:06 +1000

    ascii art

--- data.py
+++ data.py
@@ -21,3 +21,35 @@ def starwars():
  .       .   \/    \/  /____/      \____\ |___|  \____________/ ADVENTURE By M ROGER[/] """, style= "yellow")
     console.rule("")
     console.rule("")
+def yoda():
+    
+    console.print(r""" [green]
+                    ____
+                 _.' :  `._
+             .-.'`.  ;   .'`.-.
+    __      / : ___\ ;  /___ ; \      __
+  ,'_ ""--.:__;".-.";: :".-.":__;.--"" _`,
+  :' `.t""--.. '<@.`;_  ',@>` ..--""j.' `;
+       `:-.._J '-.-'L__ `-- ' L_..-;'
+         "-.__ ;  .-"  "-.  : __.-"
+             L ' /.------.\ ' J
+              "-.   "--"   .-"
+             __.l"-:_JL_;-";.__
+        [/]  """, style= "green")  
+def vadar():
+    console.print(r"""[red]
+          _________
+          III| |III
+        IIIII| |IIIII
+       IIIIII| |IIIIII
+       IIIIII| |IIIIII
+      IIIIIII| |IIIIIII
+      IIIIIII\ /IIIIIII
+     II (___)| |(___) II
+    II  \    /D\    /  II
+   II  \ \  /| |\  / /  II
+  II    \_\/|| ||\/_/    II
+ II     / O-------O \     II
+II_____/   \|||||/   \_____II 
+
+[/]""", style ="red")    
