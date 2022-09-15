ddab5a0155d3ec552b05f16a4da72bea4b2d77d5
Mitchell Roger <roger.mitch27@gmail.com>
2022-09-16 08:31:28 +1000

    fix quiz data

--- data.py
+++ data.py
@@ -7,69 +7,42 @@ question_data = [
         
     },
     {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "medium",
-        "question": "The first computer bug was formed by faulty wires.",
-        "correct_answer": "False"
+       "question": "You find a wallet on the sidewalk filled with cash and the owner's driver's license. What do you do?:\n\nA. Send the wallet back to the owner\nB. send the wallet back to the owner but keep the money\n ",
+        "correct_answer": "A"
         
     },
     {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "medium",
-        "question": "FLAC stands for 'Free Lossless Audio Condenser'.",
-        "correct_answer": "False"
+        "question": "\nHow do you get someone you find attractive to like you?:\n\nA. Seduce them by telling them what they want to hear\nB. talk to them and find out more about their intrest and peronality",
+        "correct_answer": "B"
        
     },
     {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "medium",
-        "question": "All program codes have to be compiled into an executable file in order to be run. This file can then be executed on any machine.",
-        "correct_answer": "False"
+       "question": "\nHow do you get someone you find attractive to like you?:\n\nA. Seduce them by telling them what they want to hear\nB. talk to them and find out more about their intrest and peronality",
+        "correct_answer": "B"
     },
     {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "Linus Torvalds created Linux and Git.",
-        "correct_answer": "True"
+        "question": "\nHow do you get someone you find attractive to like you?:\n\nA. Seduce them by telling them what they want to hear\nB. talk to them and find out more about their intrest and peronality",
+        "correct_answer": "B"
         
     },
     {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "The programming language 'Python' is based off a modified version of 'JavaScript'",
-        "correct_answer": "False"
+       "question": "\nHow do you get someone you find attractive to like you?:\n\nA. Seduce them by telling them what they want to hear\nB. talk to them and find out more about their intrest and peronality",
+        "correct_answer": "B"
     },
     {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "medium",
-        "question": "AMD created the first consumer 64-bit processor.",
-        "correct_answer": "True"
+       "question": "\nHow do you get someone you find attractive to like you?:\n\nA. Seduce them by telling them what they want to hear\nB. talk to them and find out more about their intrest and peronality",
+        "correct_answer": "B"
     },
     {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "'HTML' stands for Hypertext Markup Language.",
-        "correct_answer": "True"
+       "question": "\nHow do you get someone you find attractive to like you?:\n\nA. Seduce them by telling them what they want to hear\nB. talk to them and find out more about their intrest and peronality",
+        "correct_answer": "B"
     },
     {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "In most programming languages, the operator ++ is equivalent to the statement '+= 1'.",
-        "correct_answer": "True"
+        "question": "\nHow do you get someone you find attractive to like you?:\n\nA. Seduce them by telling them what they want to hear\nB. talk to them and find out more about their intrest and peronality",
+        "correct_answer": "B"
     },
     {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "hard",
-        "question": "The IBM PC used an Intel 8008 microprocessor clocked at 4.77 MHz and 8 kilobytes of memory.",
-        "correct_answer": "False"
+        "question": "\nHow do you get someone you find attractive to like you?:\n\nA. Seduce them by telling them what they want to hear\nB. talk to them and find out more about their intrest and peronality",
+        "correct_answer": "B"
     }
 ]
